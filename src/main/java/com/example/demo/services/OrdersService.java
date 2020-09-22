003d63112645e03b7341141f5523154f8466b948
Denis Shaporov <denisrox@mail.ru>
2020-09-23 01:59:43 +0300

    дошел до рабочей точки

---
+++ src/main/java/com/example/demo/services/OrdersService.java
@@ -0,0 +1,15 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Currency;
+import com.example.demo.repositories.ICurrencyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+
+public class OrdersService {
+}
